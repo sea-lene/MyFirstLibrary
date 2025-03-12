7a6647d954363108f9a83e9548cc97449787ceba
Selene <suhaneemavar01@gmail.com>
2025-03-12 07:07:01 +0530

    Initial First commit - Swift Package

---
+++ Sources/MyFirstLibrary/MyFirstLibrary.swift
@@ -0,0 +1,8 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+public struct MyLibray{
+    public static func greet() -> String{
+        return "Hello , This is my first library"
+    }
+}
